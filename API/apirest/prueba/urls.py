38aeaf095934f0c4819730de4e9c03c48e850993
Kendovvul <sebastian.martinezca@sansano.usm.cl>
2021-11-28 19:00:44 -0300

    a

--- API/apirest/prueba/urls.py
+++ API/apirest/prueba/urls.py
@@ -2,8 +2,7 @@ from .views import *
 from django.urls import path
 
 urlpatterns = [
+	path('CambiarImagen', CambiarPerfil.as_view()),
 	path('', ResultadosVista.as_view()),
 	path('hola',inicio),
-	path('CambiarImagen', CambiarPerfil.as_view()),
-	
 ]
